b58e2d05a50d32b63ac887ec67098450c2174fc4
Samuli Suomi <sasuomi@gmail.com>
2018-12-09 14:29:30 +0200

    Make header pretty

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends PureComponent {
           <div className="App">
             <header className="App-header">
               <h1>
-                Giftibutor v0.1
+                Giftibutor
               </h1>
             </header>
             <main className="App-main">
