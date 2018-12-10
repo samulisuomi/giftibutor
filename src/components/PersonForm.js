525556953ce385bd9b0771c890ee95680e0c0417
Samuli Suomi <sasuomi@gmail.com>
2018-12-10 17:43:55 +0200

    Reorganize files, add constants

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import './PersonForm.css';
 
-import { encrypt } from './utils';
+import { encrypt } from '../utilities/utils';
 
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
