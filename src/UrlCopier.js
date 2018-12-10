0dd788acff9fc51a1fce965827773df56ea77441
Samuli Suomi <sasuomi@gmail.com>
2018-12-10 17:35:08 +0200

    Add clipboard copier

--- src/UrlCopier.js
+++ src/UrlCopier.js
@@ -1,9 +1,13 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
+import { withRouter } from 'react-router'
+import Clipboard from 'react-clipboard.js';
+
 class UrlCopier extends PureComponent {
   static propTypes = {
-    giftee: PropTypes.string
+    giftee: PropTypes.string,
+    router: PropTypes.func.isRequired
   }
 
   render() {
@@ -12,9 +16,11 @@ class UrlCopier extends PureComponent {
     } = this.props;
 
     return giftee ? (
-      <span>{ giftee }</span>
+      <Clipboard data-clipboard-text={ `https://sasuomi.github.io/giftibutor?your_giftee=${giftee}` }>
+        Copy Link to Clipboard
+      </Clipboard>
     ) : null;
   }
 }
 
-export default UrlCopier;
+export default withRouter(UrlCopier);
