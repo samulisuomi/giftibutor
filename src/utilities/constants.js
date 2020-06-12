ed59c450069b64019b366c303a2a466e9c109b86
Samuli Suomi <3490745+samulisuomi@users.noreply.github.com>
2020-06-12 09:48:37 +0300

    Fix changed URL

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,5 +1,5 @@
 export const ROOT_URL_DEV = 'http://localhost:3000';
-export const ROOT_URL_PROD = 'https://sasuomi.github.io/giftibutor';
+export const ROOT_URL_PROD = 'https://samulisuomi.github.io/giftibutor';
 
 export default {
   ROOT_URL_DEV,
